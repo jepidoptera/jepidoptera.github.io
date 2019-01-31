b05d48d44ec7dac4d33afecbe898962404e45d3e
jepidoptera <jepidoptera@live.com>
2019-01-31 15:29:08 -0600

    implemented header scrolling? let's see.

--- scripts/about.js
+++ scripts/about.js
@@ -1,8 +1,10 @@
 var mainWindow;
 // get a reference to the main window
 window.addEventListener('message', function(event){
-    mainWindow = event.source;
-    console.log("got reference");
+    if (event.data == "hi"){
+        mainWindow = event.source;
+        console.log("got reference");
+    }
 });
 document.addEventListener("scroll", function (){
     mainWindow.postMessage(pageYOffset);
