9aba0da89846a0bbd725e0fc87452fb7cc6d1c9a
jepidoptera <jepidoptera@live.com>
2019-02-01 17:14:12 -0600

    implemented header scrolling? let's see.

--- scripts/main.js
+++ scripts/main.js
@@ -13,44 +13,45 @@ var iframeTop = 0;
 var iframe;
 // var iframe;
 function loadIframe(){
-    document.getElementById("headerSection").addEventListener("click", function() {
-        document.getElementById("headerSection").style.top = "10px";
-    });
-    
-        // // send a message to the iframe so it can get a handle to the parent window
-    // var iframe = document.getElementById('iframePanel');
-    // iframe.contentWindow.postMessage("hi", "*");
-    // get the default height and position for the iframe
-    iframe =  document.getElementById("iframePanel");
-    iframeHeight = iframe.style.height;
-    iframeTop = iframe.style.top;
+    if (screen.width < 640){
+        iframe =  document.getElementById("iframePanel");
+        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+    }
 
-    // let's try this a different way...
-    iframe.contentWindow.addEventListener("scroll", function() {
-        if (screen.width <= 640) {
-            // phone screen
-            // move header out of the way
-            var header = document.getElementById("headerSection");
-            var iframe = document.getElementById('iframePanel');
-            var scrollHeight = parseInt(iframe.contentWindow.pageYOffset);
-            if (scrollHeight == 0) {
-                document.getElementById("headerSection").style.top = "0px";
-                return;
-            }
-            var headerMin = -parseInt(header.style.height);
-            var headerTop = Math.max(parseInt(header.style.top) - toString(-scrollHeight), headerMin);
-            document.getElementById("headerSection").style.top = headerTop + "px";
-            if (headerTop > headerMin) {
-                iframe.contentWindow.pageYOffset = 0;
-            }
-            iframe.style.height = Math.max(
-                iframeHeight + parseInt(event.data), iframeHeight + header.style.height
-            ) + "px";
-            iframe.style.top = Math.min(
-                iframeTop - parseInt(event.data), 0
-            ) + "px";
-        }
-    });
+    // // // send a message to the iframe so it can get a handle to the parent window
+    // // var iframe = document.getElementById('iframePanel');
+    // // iframe.contentWindow.postMessage("hi", "*");
+    // // get the default height and position for the iframe
+    // iframe =  document.getElementById("iframePanel");
+    // iframeHeight = iframe.style.height;
+    // iframeTop = iframe.style.top;
+
+    // // let's try this a different way...
+    // iframe.contentWindow.addEventListener("scroll", function() {
+    //     if (screen.width <= 640) {
+    //         // phone screen
+    //         // move header out of the way
+    //         var header = document.getElementById("headerSection");
+    //         var iframe = document.getElementById('iframePanel');
+    //         var scrollHeight = parseInt(iframe.contentWindow.pageYOffset);
+    //         if (scrollHeight == 0) {
+    //             document.getElementById("headerSection").style.top = "0px";
+    //             return;
+    //         }
+    //         var headerMin = -parseInt(document.getElementById("headerSection").style.height);
+    //         var headerTop = Math.max(parseInt(document.getElementById("headerSection").style.top) - toString(-scrollHeight), headerMin);
+    //         document.getElementById("headerSection").style.top = headerTop + "px";
+    //         if (headerTop > headerMin) {
+    //             iframe.contentWindow.pageYOffset = 0;
+    //         }
+    //         iframe.style.height = Math.max(
+    //             iframeHeight + parseInt(event.data), iframeHeight + header.style.height
+    //         ) + "px";
+    //         iframe.style.top = Math.min(
+    //             iframeTop - parseInt(event.data), 0
+    //         ) + "px";
+    //     }
+    // });
 }
 
 // window.addEventListener("message", function(event){
