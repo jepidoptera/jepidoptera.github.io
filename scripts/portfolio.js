c37f9823abbbd93ecba5ed4413c454137a09ecb2
jepidoptera <jepidoptera@live.com>
2019-02-04 15:48:29 -0600

    updated random words submodule

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,5 +1,8 @@
 function randomTextButton(){
-    window.open("random-words.html", "iframePanel");
+    window.open("random-text-generator/index.html", "iframePanel");
+}
+function rpsButton(){
+    window.open("rock-paper-scissors/index.html", "iframePanel");
 }
 function questionButton(){
     window.open("coming-soon.html", "iframePanel");
