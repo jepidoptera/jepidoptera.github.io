2cbf63c82ba2740404849275d32ef06c7981555e
jepidoptera <jepidoptera@live.com>
2019-02-05 15:57:14 -0600

    nailed formatting

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,41 @@
 // open links inside innerDiv when the buttons are pushed
-function clickContact() {
-    // loadInnerDiv("contact.html");
-    window.open("contact.html", "iframePanel");
-}
-function clickAbout() {
-    window.open("about.html", "iframePanel");
-    // loadinnerDiv("about.html");
-}
-function clickPortfolio() {
-    window.open("portfolio.html", "iframePanel");
-    // loadinnerDiv("portfolio.html");
-}
+// function clickContact() {
+//     // loadInnerDiv("contact.html");
+//     window.open("contact.html", "iframePanel");
+// }
+// function clickAbout() {
+//     window.open("about.html", "iframePanel");
+//     // loadinnerDiv("about.html");
+// }
+// function clickPortfolio() {
+//     window.open("portfolio.html", "iframePanel");
+//     // loadinnerDiv("portfolio.html");
+// }
 
-// var innerDiv;
-function loadIframe(){
-    var iframe = document.getElementById("iframePanel");
-    // get cross-platform screen width (thanks, stack overflow)
-    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+// // var innerDiv;
+// function loadIframe(){
+//     var iframe = document.getElementById("iframePanel");
+//     // get cross-platform screen width (thanks, stack overflow)
+//     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
-    // cross-platform document height
-    var body = iframe.contentDocument.body,
-    html = iframe.contentDocument.documentElement;
+//     // cross-platform document height
+//     var body = iframe.contentDocument.body,
+//     html = iframe.contentDocument.documentElement;
 
-    var height = Math.max( body.scrollHeight, body.offsetHeight, 
-        html.clientHeight, html.scrollHeight, html.offsetHeight );
+//     var height = Math.max( body.scrollHeight, body.offsetHeight, 
+//         html.clientHeight, html.scrollHeight, html.offsetHeight );
 
-    if (iframe.contentWindow.location.href.slice(-12) == "contact.html"){
-        iframe.style.height = "Calc(100vh - 140px)";
-        iframe.contentDocument.body.style.overflow = "hidden";
-    }
-    else if (w < 640) {
-        iframe.style.height = height + "px";
-        iframe.contentDocument.body.style.overflow = "hidden";
-    }
-    else {
-        iframe.style.height = "Calc(100vh - 140px)";
-        iframe.contentDocument.body.style.overflow = "scroll";
-    }
-}
+//     if (iframe.contentWindow.location.href.slice(-12) == "contact.html"){
+//         iframe.style.height = "Calc(100vh - 140px)";
+//         iframe.contentDocument.body.style.overflow = "hidden";
+//     }
+//     else if (w < 640) {
+//         iframe.style.height = height + "px";
+//         iframe.contentDocument.body.style.overflow = "hidden";
+//     }
+//     else {
+//         iframe.style.height = "Calc(100vh - 140px)";
+//         iframe.contentDocument.body.style.overflow = "scroll";
+//     }
+// }
 
