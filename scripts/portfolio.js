b0e8c30eedf0d8e44700d4920259caaa8e2cd1e2
jepidoptera <jepidoptera@live.com>
2019-02-08 11:35:16 -0600

    minor formatting adjustments, added hangman to portfolio

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,9 +1,12 @@
 function randomTextButton(){
-    window.open("random-text-generator/index.html", "iframePanel");
+    window.open("random-text-generator/index.html", "_blank");
 }
 function rpsButton(){
-    window.open("rock-paper-scissors/index.html", "iframePanel");
+    window.open("rock-paper-scissors/index.html", "_blank");
+}
+function hangmanButton(){
+    window.open("hangman/index.html", "_blank");
 }
 function questionButton(){
-    window.open("coming-soon.html", "iframePanel");
+    window.open("coming-soon.html", "_blank");
 }
